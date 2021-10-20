c3217a89fbbbe332bb779346b18f7eae4aa563d6
domishq <91064596+domishq@users.noreply.github.com>
2021-10-20 19:51:38 +0200

    Add files via upload

---
+++ teka_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int z,x;
+  cout<<"Unesite neki broj x: ";
+  cin>>x;
+  if(x>0)
+  {
+    z=1;
+  }
+  else
+  {
+    z=0;
+  }
+cout<<z<<endl;
+return 0;
+}
